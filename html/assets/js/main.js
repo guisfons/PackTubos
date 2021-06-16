4735955f0a6afc97f14148966141212013ac2c48
Guilherme Fonseca <guilherme.silva@okn.com.br>
2021-06-15 22:25:00 -0300

    Progresso Projeto | Slider e Section 'A Empresa'

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('.loading').hide();
+
+    const slider = function() {
+        $('.main__slider-slides').slick({
+            dots: false,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        });
+    }
+    slider();
 })
