61e597723306ec0b55619e87ebfff65e1737b9cf
Guilherme Fonseca <guilherme.silva@okn.com.br>
2021-08-15 19:46:21 -0300

    Site finalizado

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -9,4 +9,31 @@ $(document).ready(function(){
         });
     }
     slider();
+
+    if($(window).width() <= 900){
+        // add menu mobile
+        $('.header__container').append(`
+            <figure class="header__mobile">
+                <span></span>
+                <span></span>
+                <span></span>
+            </figure>
+        `)
+
+        const headerNav = $('.header__nav');
+        const menuMobile = $('.header__mobile');
+        menuMobile.on('click', () => {
+            menuMobile.addClass('header__mobile--active')
+            $('body').addClass('menu-open')
+            $('body').append(headerNav)
+            headerNav.addClass('header__nav--open')
+            headerNav.append('<span class="header__close"></span>')
+
+            $('.header__close').on('click', () => {
+                menuMobile.removeClass('header__mobile--active')
+                $('body').removeClass('menu-open')
+                headerNav.removeClass('header__nav--open')
+            })
+        })
+    }
 })
--- www/single.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $title = ;
-    include 'header.php';
-    
-    include 'footer.php';
-?>
